ca13371106596be022a11b255bf708689acd4452
apierce1992 <apierce1992@gmail.com>
2016-01-18 11:10:22 -0500

    removed duplicate possibility by shuffling array

--- powerball.pl
+++ powerball.pl
@@ -1,13 +1,19 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use List::Util qw(shuffle);
+
+my @data = (1..69);
+
+my @white_nums = shuffle @data;
+
 
 sub pick_lotto_nums {
 my $random_number_red = int(rand(25)) + 1;
 my @lotto_nums = ();
 for (my $i =1; $i <= 5; $i++) {
-  my $random_number_white = int(rand(68)) + 1;
-  push @lotto_nums, $random_number_white;
+  my $chosen_white = pop @white_nums;
+  push @lotto_nums, $chosen_white;
 }
 push @lotto_nums, $random_number_red;
 print "@lotto_nums";
